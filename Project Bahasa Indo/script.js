79c6cad8fc29ab867d7d4905957baa06ac83e884
I Putu Mahendra Arya Valentino <138369170+Aryndra@users.noreply.github.com>
2023-10-30 13:11:09 +0800

    First Commit
    Adding items to the repository

---
+++ Project Bahasa Indo/script.js
@@ -0,0 +1,44 @@
+// loader
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector(".loader").style.visibility = "visible";
+  } else {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("header");
+  var scrolled = window.scrollY > 2;
+  if (scrolled) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// scroll nav
+// let section = document.querySelectorAll("section");
+// let navLinks = document.querySelectorAll("nav a");
+// window.onscroll = () => {
+//   section.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       navLinks.forEach((links) => {
+//         links.classList.remove("active");
+//         document
+//           .querySelector("header link a[href*=" + id + "]")
+//           .classList.add("active");
+//       });
+//     }
+//   });
+// };
+$(document).ready(function () {
+  $("body").scrollspy({ target: ".navbar", offset: 50 });
+});
