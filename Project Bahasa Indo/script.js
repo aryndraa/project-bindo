5058b8e993784c089e8958054247dfc8fe49d0b0
I Putu Mahendra Arya Valentino <138369170+Aryndra@users.noreply.github.com>
2023-10-30 22:41:34 +0800

    Add files via upload

--- Project Bahasa Indo/script.js
+++ Project Bahasa Indo/script.js
@@ -1,13 +1,58 @@
+// loader
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector(".loader").style.visibility = "visible";
+  } else {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("header");
+  var scrolled = window.scrollY > 2;
+  if (scrolled) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// scroll nav
+// let section = document.querySelectorAll("section");
+// let navLinks = document.querySelectorAll("nav a");
+// window.onscroll = () => {
+//   section.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       navLinks.forEach((links) => {
+//         links.classList.remove("active");
+//         document
+//           .querySelector("header link a[href*=" + id + "]")
+//           .classList.add("active");
+//       });
+//     }
+//   });
+// };
+$(document).ready(function () {
+  $("body").scrollspy({ target: ".navbar", offset: 50 });
+});
+
 // === Profil Start
 
 // Infinite Scrolling
-const innerScroller = document.querySelector('.inner-scroller');
+const innerScroller = document.querySelector(".inner-scroller");
 const scrollerContent = Array.from(innerScroller.children);
 
-scrollerContent.forEach(item => {
-    const duplicatedItem = item.cloneNode(true);
-    duplicatedItem.setAttribute('aria-hidden', true);
-    innerScroller.appendChild(duplicatedItem);
-})
+scrollerContent.forEach((item) => {
+  const duplicatedItem = item.cloneNode(true);
+  duplicatedItem.setAttribute("aria-hidden", true);
+  innerScroller.appendChild(duplicatedItem);
+});
 
 // === Profil End
